29d74ade16ee04006a00f96c8b43ebc52ef54dd8
LíJimpéy(： <jimpey88@outlook.com>
2024-07-21 12:23:47 +0800

    Update _worker.js uuid cahnged

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '2427af55-7e5c-4479-8d01-1de9294843d4';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
